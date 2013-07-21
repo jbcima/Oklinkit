ce3529b66c57c1fe6745f8b2907983954b1410d0
jbcima <me@jeremycimafonte.com>
2013-07-21 03:49:30 -0400

    added shit

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var app = module.exports = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+// Use Hogan.js as view template.
+app.set('view engine', 'hjs');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
