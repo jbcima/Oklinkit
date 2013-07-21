3fe52ed59b19ec93a78457f35545c99161863431
Alan <linpd2@gmail.com>
2013-07-21 03:39:47 -0400

    added facebook graph friend request

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ var express = require('express'),
   api = require('./routes/api'),
   http = require('http'),
   path = require('path'),
-  FacebookClient = require("facebook-client").FacebookClient;
+  qs = require('querystring'),
+  request = require('request');
 
 var app = module.exports = express();
-
+var accessKey = null;
 /**
  * Configuration
  */
@@ -26,14 +27,10 @@ app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
+// fb config stuff
 var app_id = "389710287795440";
-var facebook_client = new FacebookClient(
-    app_id, // configure like your fb app page states
-    "57f02c87927967b10e6750f51431d7a9", // configure like your fb app page states
-    {
-        "timeout": 10000 // modify the global timeout for facebook calls (Default: 10000)
-    }
-);
+var app_secret = "57f02c87927967b10e6750f51431d7a9";
+
 // development only
 if (app.get('env') === 'development') {
   app.use(express.errorHandler());
@@ -49,46 +46,50 @@ if (app.get('env') === 'production') {
  * Routes
  */
 
-
 // serve index and view partials
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 
 // JSON API
 app.get('/api/name', api.name);
-app.get('/hello', function(request, response) {
 
-facebook_client.getSessionByRequestHeaders(request.headers)(function(facebook_session) {
-        if (!facebook_session)
-        {
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write([
-                '<html>',
-                '<head><title>node-facebook-client example</title></head><body>',
-                '<p>Login please</p> <fb:login-button autologoutlink="true"></fb:login-button>',
-                '<div id="fb-root"></div>',
-                '<script type="text/javascript">',
-                  'window.fbAsyncInit = function() {',
-
-                  '    FB.init({appId: "' + app_id +'", logging:false, status: true, cookie: true, xfbml: true});',
-                  '    FB.Event.subscribe(\'auth.sessionChange\', function(response) {',
-                  '        document.location = document.location.href;',
-                  '    });',
-                  '};',
-                  '(function() {',
-                  '  var e = document.createElement(\'script\'); e.async = true;',
-                  '  e.src = document.location.protocol +',
-                  '    \'//connect.facebook.net/en_US/all.js\';',
-                  '  document.getElementById(\'fb-root\').appendChild(e);',
-                  '}());',
-                  '</script>',
-                  '</body>',
-                  '</html>'
-            ].join("\n"));
-            response.end();
-            return ;
-        }
+// Get the access code
+app.get('/api/fb', function(req, res) {
+  var url = "https://www.facebook.com/dialog/oauth?"+
+  "client_id=389710287795440&"+
+  "redirect_uri=http://localhost:3000/api/fb/callback";
+  res.redirect(url);
 });
+
+// Get the access token
+app.get('/api/fb/callback', function(req, res) {
+  var code = req.param('code');
+
+  var url = "https://graph.facebook.com/oauth/access_token?"+
+    "client_id=389710287795440&redirect_uri="+
+    "http://localhost:3000/api/fb/callback&"+
+    "client_secret=57f02c87927967b10e6750f51431d7a9&code=" + code;
+
+  request.get(url, function (error, response, body) {
+    if(response.statusCode == 200) {
+      console.log("Setting key");
+      accessKey = qs.parse(body).access_token;
+    } else {
+      console.log("Sorry, error getting access token");
+    }
+  });
+});
+
+// Get fb friends
+app.get('/fb/friends', function(req, res) {
+  // Get limit
+  var limit = req.param('limit') || 10;
+  var url = "https://graph.facebook.com/me/friends?access_token=" +
+    accessKey + "&limit=" + limit;
+      request.get(url, function(err, res, body) {
+        var json = JSON.parse(body);
+        console.log(json);
+      });
 });
 
 // redirect all others to the index (HTML5 history)
