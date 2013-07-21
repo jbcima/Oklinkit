6ac806f3b14bbce565c3d1c2680407fc7e4c4fab
jbcima <me@jeremycimafonte.com>
2013-07-21 12:58:24 -0400

    pres. stuff

--- public/js/activity.js
+++ public/js/activity.js
@@ -13,14 +13,14 @@ $('#activity').on('click',function(){
 			$.get('http://localhost:3000/users/responses/me', function(data) {
 					$.each(data, function(key, data){
 						$.each(data.responses, function(key, data){
-						$('#feed-responses').append('<li><div class="info"><img src="http://placehold.it/100x100" class="rounded"><h2>' + data.name + '</h2><p>“' + data.body + '”</p><a href="mailto:' + data.author + '">' + data.author + '</a><br /><span><i class="icon-trophy"></i> ' + Math.floor((Math.random()*5)+1) +' &nbsp; <i class="icon-frown"></i> ' + Math.floor((Math.random()*5)+1) +'</span></div></li>');
+						$('#feed-responses').append('<li><div class="info"><img src="https://profile-b.xx.fbcdn.net/hprofile-ash4/c12.12.156.156/s100x100/1005311_10201185469335496_586304505_a.jpg" class="rounded"><h2>' + data.name + '</h2><p>“' + data.body + '”</p><a href="mailto:' + data.author + '">' + data.author + '</a><br /><span><i class="icon-trophy"></i> ' + Math.floor((Math.random()*5)+1) +' &nbsp; <i class="icon-frown"></i> ' + Math.floor((Math.random()*5)+1) +'</span></div></li>');
 						});
 					});
 			});	
 
 			$.get('http://localhost:3000/users/requests/me', function(data) {
 				$.each(data, function(key, data){
-				$('#feed-requests').append('<li><div class="info"><div class="rqstbx cf"><span class="yes-btn">✔</span><span class="no-btn">✖</span></div><img src="http://placehold.it/100x100" class="rounded"><h2>' + data.request.name + '</h2><p>“' + data.request.description + '”</p><a href="mailto:' + data.request.author + '">' + data.request.author + '</a><br /><span><i class="icon-trophy"></i> ' + Math.floor((Math.random()*5)+1) +' &nbsp; <i class="icon-frown"></i> ' + Math.floor((Math.random()*5)+1) +'</span></div></li>');
+				$('#feed-requests').append('<li><div class="info"><div class="rqstbx cf"><span class="yes-btn">✔</span><span class="no-btn">✖</span></div><img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash3/c9.9.113.113/s100x100/581705_10151527993128698_567010700_s.jpg" class="rounded"><h2>' + data.request.name + '</h2><p>“' + data.request.description + '”</p><a href="mailto:' + data.request.author + '">' + data.request.author + '</a><br /><span><i class="icon-trophy"></i> ' + Math.floor((Math.random()*5)+1) +' &nbsp; <i class="icon-frown"></i> ' + Math.floor((Math.random()*5)+1) +'</span></div></li>');
 				});
 			});	
 
@@ -32,6 +32,7 @@ $('#activity').on('click',function(){
 
 $('#requests').on('click',function(){
 	$('#feed-responses').fadeOut(function(){
+		$('.intro').text('people think you would be good at these:').fadeIn();
 		$('#feed-requests').fadeIn();
 	});
 
@@ -40,6 +41,8 @@ $('#requests').on('click',function(){
 });
 
 $('#responses').on('click',function(){
+		$('.intro').text('people who are interested:').fadeIn();
+	
 	$('#feed-requests').fadeOut(function(){
 		$('#feed-responses').fadeIn();
 	});
